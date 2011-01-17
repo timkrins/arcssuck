e0ce6dc8be5bed0b0b00f3832296a0550a358f17
timkrins <timkrins@gmail.com>
2011-01-17 12:50:21 +1100

    Old issue was memory not being cleared, oops. Fixed. Will rerun test challenges.

--- arcssuck.c
+++ arcssuck.c
@@ -6,16 +6,16 @@
 void G02_print(char casenumber, float start_x, float start_y, float radius, FILE *fp, int l);
 void G03_print(char casenumber, float start_x, float start_y, float radius, FILE *fp, int l);
 char do_Determine(float start_x, float start_y, float end_x, float end_y);
-void do_Calculate(char type, float start_x, float start_y, float end_x, float end_y, float radius, char rez, FILE *fp, int l);
+void do_Calculate(char typer, float start_x, float start_y, float end_x, float end_y, float radius, char rez, FILE *fp, int l);
 
-volatile float listarray[1000][2];
+float listarray[1000][2];
 int x_positive, y_positive;
 int l = 1;
-int rez, amt_steps, curstep, stepcalc, j, i, y_switch, x_direction, y_direction;
+int rez, amt_steps, curstep, stepcalc, y_switch, x_direction, y_direction;
 //float start_x, start_y, end_x, end_y, radius
 float temp_x, temp_y, GX, GY;
 float yval, resolution, currentx, arc_number, arc_value;
-int i, j, k, GCLen, XCLen, YCLen, RCLen, GFirst, XFirst, YFirst, RFirst, GVal;
+int i, j, k, m, n, GCLen, XCLen, YCLen, RCLen, GFirst, XFirst, YFirst, RFirst, GVal;
 char GString[10], XString[16], YString[16], RString[16];
 float XVal, YVal, RVal, XLast, YLast = (float)0;
 
@@ -31,10 +31,12 @@ int main ( void )
       while ( fgets(line, sizeof line, file)) /* read a line */
       {
 				//fputs ( line, stdout ); /* write the line */
+				GCLen, XCLen, YCLen, RCLen, GVal = 0;
+				memset (listarray, 0, 1000);
 				for (i = 0; line[i]; i++) {
 				//entire line is now in memory
 				//reset variables
-				GCLen, XCLen, YCLen, RCLen = 0;
+				
 					 if(line[i] == 'G' || line[i] == 'g') {
 					 //if a G is encountered, I want to count the amount of digits before a space.
 					 GFirst = i;
@@ -98,7 +100,7 @@ int main ( void )
       }
       else
       {
-      printf("Type %d.\n\n", GVal);
+      printf("typer %d.\n\n", GVal);
       fprintf(fp, line);
       if(XCLen) {XLast = XVal;}
       if(YCLen) {YLast = YVal;}
@@ -118,29 +120,32 @@ int main ( void )
 
 }
 
-void do_Calculate(char type, float start_x, float start_y, float end_x, float end_y, float radius, char rez, FILE *fp, int l) {
+void do_Calculate(char typer, float start_x, float start_y, float end_x, float end_y, float radius, char rez, FILE *fp, int l) {
 stepcalc = 100000;
+
+if(!rez) {rez=1;}
+float resolution = ((float)1/rez);
+
 if ((end_x-start_x) < stepcalc) {
 stepcalc = (end_x-start_x);
 y_switch = 0;
 }
-if(!rez) {rez=1;}
-float resolution = ((float)1/rez);
+
 if ((end_y-start_y) < stepcalc) {
 stepcalc = (end_y-start_y);
 y_switch = 1;
 }
 amt_steps = abs(stepcalc*rez);
-printf("Amount of steps: %d.\n", amt_steps, y_switch, x_direction, y_direction);
-for (i = 0; i <= amt_steps; i++) {
-arc_number = (i*resolution);
+printf("Amount of steps: %d.\n%d %.0f %.0f %.0f %.0f %.0f %d\n", amt_steps, typer, start_x, start_y, end_x, end_y, radius, rez);
+for (m = 0; m <= amt_steps; m++) {
+arc_number = (m*resolution);
 arc_value = radius-sqrt((radius*radius)-((arc_number*arc_number)));
-listarray[i][0] = arc_number+1;
-listarray[i][1] = arc_value+1;
+listarray[m][0] = arc_number+1;
+listarray[m][1] = arc_value+1;
 }
-printf("Type %d.\n\n", type);
-if (type == 2) {G02_print(do_Determine(start_x, start_y, end_x, end_y), start_x, start_y, radius,fp, l);}
-if (type == 3) {G03_print(do_Determine(start_x, start_y, end_x, end_y), start_x, start_y, radius,fp, l);}
+printf("typer %d.\n\n", typer);
+if (typer == 2) {G02_print(do_Determine(start_x, start_y, end_x, end_y), start_x, start_y, radius,fp, l);}
+if (typer == 3) {G03_print(do_Determine(start_x, start_y, end_x, end_y), start_x, start_y, radius,fp, l);}
 }
 
 char do_Determine(float start_x, float start_y, float end_x, float end_y) {
@@ -150,23 +155,23 @@ return (x_positive+y_positive);
 }
 
 void G03_print(char casenumber, float start_x, float start_y, float radius, FILE *fp, int l) {
-for (j = 0; listarray[j][0]; j++) {
+for (n = 0; listarray[n][0]; n++) {
 switch(casenumber) {
   case 5: // X pos, Y pos
-    GX = (listarray[j][0]-1)+start_x;
-    GY = (listarray[j][1]-1)+start_y;
+    GX = (listarray[n][0]-1)+start_x;
+    GY = (listarray[n][1]-1)+start_y;
   break;
   case 8: // X pos, Y neg
-  GX = (listarray[j][1]-1)+start_x;
-  GY = (start_y-(listarray[j][0]-1));
+  GX = (listarray[n][1]-1)+start_x;
+  GY = (start_y-(listarray[n][0]-1));
   break;
   case 6: // X is negative, Y is positive
-    GX = radius+(start_y-(listarray[j][1]-1));
-    GY = start_x-(listarray[amt_steps-j][0]-1);
+    GX = radius+(start_y-(listarray[n][1]-1));
+    GY = start_x-(listarray[amt_steps-n][0]-1);
   break;
   case 9: // X neg, Y neg
-    GX = start_x-(listarray[j][0]-1);
-    GY = start_y-(listarray[j][1]-1);
+    GX = start_x-(listarray[n][0]-1);
+    GY = start_y-(listarray[n][1]-1);
   break;
   default:
     printf("screwed up\n");
@@ -177,23 +182,23 @@ switch(casenumber) {
 }
 
 void G02_print(char casenumber, float start_x, float start_y, float radius, FILE *fp, int l) {
-    for (j = 0; listarray[j][0]; j++) {
+    for (n = 0; listarray[n][0]; n++) {
 switch(casenumber) {
   case 5: // X pos, Y pos
-    GX = (listarray[j][1]-1)+start_x;
-    GY = (listarray[j][0]-1)+start_y;
+    GX = (listarray[n][1]-1)+start_x;
+    GY = (listarray[n][0]-1)+start_y;
   break;
   case 8: // X pos, Y neg
-    GX = (listarray[j][0]-1)+start_x;
-    GY = start_y-(listarray[j][1]-1);
+    GX = (listarray[n][0]-1)+start_x;
+    GY = start_y-(listarray[n][1]-1);
   break;
   case 6: // X is negative, Y is positive
-    GX = listarray[amt_steps-j][0]-1;
-    GY = start_y+(listarray[j][1]-1);
+    GX = listarray[amt_steps-n][0]-1;
+    GY = start_y+(listarray[n][1]-1);
   break;
   case 9: // X neg, Y neg
-    GX = start_x-(listarray[j][1]-1);
-    GY = start_y-(listarray[j][0]-1);
+    GX = start_x-(listarray[n][1]-1);
+    GY = start_y-(listarray[n][0]-1);
   break;
   default:
     printf("screwed up\n");
